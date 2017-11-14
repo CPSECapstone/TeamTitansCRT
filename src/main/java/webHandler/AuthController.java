70ce986247bc38041e2fa638b6f6d4acac406fc3
Robert Weber <RAWeber95@gmail.com>
2017-11-14 10:12:25 -0800

    Moved IAM key into request body

--- src/main/java/webHandler/AuthController.java
+++ src/main/java/webHandler/AuthController.java
@@ -3,16 +3,16 @@ package webHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
 
 @RestController
 public class AuthController {
 
-    @RequestMapping(value = "/auth/{key}", method = RequestMethod.POST)
-    public ResponseEntity<String> Authenticate(@PathVariable("key") String key) {
-        return new ResponseEntity<String>(key, HttpStatus.OK);
+    @RequestMapping(value = "/auth", method = RequestMethod.POST)
+    public ResponseEntity<String> Authenticate(@RequestBody String key) {
+        return new ResponseEntity<String>("IAM Key: " + key, HttpStatus.OK);
     }
 
 }
