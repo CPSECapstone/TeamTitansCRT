d275a573a1b278ab2dc864e7623fbb9f79098162
Michael McAniff <michael.mcaniff@gmail.com>
2017-11-28 08:34:58 -0800

    Added to CloudWatchManager to get statistics for a specific metric.

--- src/main/java/webHandler/CloudWatchManager.java
+++ src/main/java/webHandler/CloudWatchManager.java
@@ -1,12 +1,16 @@
 package webHandler;
 
 import com.amazonaws.SdkClientException;
-import com.amazonaws.auth.AWSCredentialsProvider;
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.cloudwatch.AmazonCloudWatch;
 import com.amazonaws.services.cloudwatch.AmazonCloudWatchClientBuilder;
+import com.amazonaws.services.cloudwatch.model.GetMetricStatisticsRequest;
+import com.amazonaws.services.cloudwatch.model.GetMetricStatisticsResult;
+import com.amazonaws.services.cloudwatch.model.StandardUnit;
+import com.amazonaws.services.cloudwatch.model.Statistic;
+
+import java.util.Date;
 
 
 public class CloudWatchManager {
@@ -17,13 +21,13 @@ public class CloudWatchManager {
         this.cw = getConnection();
     }
 
-    public AmazonCloudWatch getConnection() throws SdkClientException{
+    public AmazonCloudWatch getConnection() throws SdkClientException {
         try {
             BasicAWSCredentials awsCredentials = new BasicAWSCredentials("access_key", "secret_key");
             AmazonCloudWatch cloudWatch = AmazonCloudWatchClientBuilder.standard()
-                    .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
                     .withRegion(Regions.US_WEST_2)
                     .build();
+
 
             return cloudWatch;
         } catch (SdkClientException e) {
@@ -31,5 +35,25 @@ public class CloudWatchManager {
         }
 
         return null;
+    }
+
+    /**
+     *
+     * @param metric The name of the metric of interest
+     * @param start  The start time
+     * @param end    The end time
+     * @return       Returns GetMetricStatisticsResult - for more information see http://docs.aws.amazon.com/AWSJavaSDK/latest/javadoc/com/amazonaws/services/cloudwatch/model/GetMetricStatisticsResult.html#withUnit-java.lang.String-
+     */
+    public GetMetricStatisticsResult getMetricStatistics(String metric, Date start, Date end) {
+        GetMetricStatisticsRequest request = new GetMetricStatisticsRequest()
+                .withNamespace("AWS/RDS")
+                .withMetricName(metric)
+                .withStartTime(start)
+                .withEndTime(end)
+                .withStatistics(Statistic.Sum, Statistic.Average)
+                .withUnit(StandardUnit.Seconds);
+
+
+        return cw.getMetricStatistics(request);
     }
 }
