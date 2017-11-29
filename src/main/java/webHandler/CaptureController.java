ac9ff8d8592c9eee3eaa0f4b37052bdce8d16ff1
Robert Weber <RAWeber95@gmail.com>
2017-11-28 16:21:05 -0800

    Hooked up RDS log and S3 storage

--- src/main/java/webHandler/CaptureController.java
+++ src/main/java/webHandler/CaptureController.java
@@ -1,5 +1,6 @@
 package webHandler;
 
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
 
+import java.io.InputStream;
 import java.util.Date;
 import java.util.HashMap;
 
@@ -45,6 +47,24 @@ public class CaptureController {
             targetCapture.setEndTime(new Date());
         } else {
             targetCapture.setEndTime(capture.getEndTime());
+        }
+
+        if (targetCapture.getStatus().equals("Finished")) {
+            String fileName = targetCapture.getId() + "-Workload.log";
+
+            // Grab RDS workload
+            RDSManager rdsManager = new RDSManager();
+            InputStream stream = rdsManager.downloadLog(targetCapture.getRds(), fileName);
+
+            if (stream == null) {
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
+
+            // Store RDS workload in S3
+            S3Manager s3Manager = new S3Manager();
+            s3Manager.uploadFile(targetCapture.getS3(), fileName, stream, new ObjectMetadata());
+
+            //TODO: Add check for file upload
         }
 
         return new ResponseEntity<>(HttpStatus.OK);
