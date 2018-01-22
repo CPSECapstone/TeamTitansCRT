62c5db4a09556ea00a92fb7156f31b64172c8ea9
ssulkar <ssulkar@calpoly.edu>
2018-01-22 01:20:31 -0800

    Added before/after (setup/teardown) to old tests

--- src/test/java/webHandler/CrtControllerTest.java
+++ src/test/java/webHandler/CrtControllerTest.java
@@ -1,37 +1,48 @@
 package webHandler;
 
+import org.junit.Before;
+import org.junit.After;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class CrtControllerTest {
+
+    private CrtController crtCon;
+
+    @Before
+    public void before() throws Exception {
+        crtCon = new CrtController();
+    }
+
     @Test
     public void index() throws Exception {
-        CrtController crtCon = new CrtController();
         assertEquals("indexPage.html", crtCon.index());
     }
 
     @Test
     public void settings() throws Exception {
-        CrtController crtCon = new CrtController();
         assertEquals("settingsPage.html", crtCon.settings());
     }
 
     @Test
     public void analyze() throws Exception {
-        CrtController crtCon = new CrtController();
         assertEquals("analyzePage.html", crtCon.analyze());
     }
     
     @Test
     public void login() throws Exception {
-        CrtController crtCon = new CrtController();
         assertEquals("setupPage.html", crtCon.login());
     }
     
     @Test
     public void dashboard() throws Exception {
-        CrtController crtCon = new CrtController();
         assertEquals("dashboardPage.html", crtCon.dashboard());
+    }
+
+    @After
+    public void after() throws Exception {
+        crtCon = null;
+        assertNull(crtCon);
     }
 }
--- src/test/java/webHandler/StatusTest.java
+++ src/test/java/webHandler/StatusTest.java
@@ -1,21 +1,36 @@
 package webHandler;
 
+import org.junit.Before;
+import org.junit.After;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class StatusTest {
+
+    private StatusMetrics statusMetrics;
+    private Status status;
+
+    @Before
+    public void before() throws Exception {
+        statusMetrics = new StatusMetrics(1.3, 2.2, 3.1);
+        status = new Status("online", statusMetrics);
+    }
+
     @Test
     public void getStatus() throws Exception {
-        StatusMetrics statusMetrics = new StatusMetrics(1.3, 2.2, 3.1);
-        Status status = new Status("online", statusMetrics);
         assertEquals("online", status.getStatus());
     }
 
     @Test
     public void getMetrics() throws Exception {
-        StatusMetrics statusMetrics = new StatusMetrics(1.3, 2.2, 3.1);
-        Status status = new Status("online", statusMetrics);
         assertNotNull(status.getMetrics());
+    }
+
+    @After
+    public void after() throws Exception {
+        statusMetrics = null;
+        status = null;
+        assertNull(status);
     }
 }
