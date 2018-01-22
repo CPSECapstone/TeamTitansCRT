ef15a99c0a2eb779d5f60bd82c1d01c0b7d43812
ssulkar <ssulkar@calpoly.edu>
2018-01-21 23:10:24 -0800

    Tests for new classes.

---
+++ src/test/java/webHandler/RDSManagerTest.java
@@ -0,0 +1,31 @@
+package webHandler;
+
+import org.junit.Test;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Scanner;
+import static org.junit.Assert.*;
+
+public class RDSManagerTest {
+
+    @Test
+    public void downloadLog() throws Exception {
+        RDSManager rdsManager = new RDSManager();
+
+        InputStream actualInput = rdsManager.downloadLog("testdb", "general/mysql-general.log");
+        Scanner s = new Scanner(actualInput).useDelimiter("\\A");
+        String actual = s.hasNext() ? s.next() : "";
+        actualInput.close();
+        s.close();
+
+        InputStream expectedInput = new FileInputStream("mysql-general.txt");
+        s = new Scanner(expectedInput).useDelimiter("\\A");
+        String expected = s.hasNext() ? s.next() : "";
+        expectedInput.close();
+        s.close();
+
+        //The actual log always appends new changes so everything won't be the same.
+        assertEquals(expected.substring(0,100), actual.substring(0,100));
+    }
+
+}
