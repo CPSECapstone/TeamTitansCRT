42cdc53d5755e82d236731cdf601742d73799973
Devon Martin <devmart10@gmail.com>
2018-01-23 18:52:19 -0800

    removes unnecessary functions

--- src/main/resources/static/js/startCapture.js
+++ src/main/resources/static/js/startCapture.js
@@ -29,61 +29,7 @@ $(document).ready(function() {
         startCapture("/capture/start", body);
     });
 
-    $("#btnCaptureStop").on("click", function() {
-        var body = {
-            id: $("#txtID").val(),
-            rds: $("#txtRDS").val(),
-            s3: $("#txtS3").val(),
-            startTime: null,
-            endTime: null,
-            status: ""
-        };
-        stopCapture("/capture/stop", body);
-    });
-
-    $("#btnStatus").on("click", function() {
-        updateStatus();
-    });
 });
-
-function updateStatus() {
-    $.ajax({
-        url: domain + "/capture/status",
-        type: "GET",
-        success: function(data) {
-            // var targetID = $("#txtID").val();
-            $('#statusTable > tbody').html("");
-            var dataString = "";
-            for (var i = 0; i < data.length; i++) {
-                var capture = data[i];
-                var id = capture["id"];
-                var status = capture["status"];
-                var noButton = "";
-                console.log("ID: " + id +
-                    "\nStatus: " + status);
-                if (status = "Running") {
-                    dataString = dataString.concat(
-                        "<tr><td>" + id +
-                        "</td><td>" + status +
-                        "</td><td>" + "<a href=\"#\" id=\"btnCaptureStop\" class=\"btn btn-default\">Stop Capture</a>" +
-                        "</td></tr>");
-                } else {
-                     dataString = dataString.concat(
-                        "<tr><td>" + id +
-                        "</td><td>" + status +
-                        "</td><td>" + noButton +
-                        "</td></tr>");
-                }
-                    
-            }
-            $('#statusTable > tbody').append(
-                dataString);
-        },
-        error: function(err) {
-            console.log(err);
-        }
-    });
-}
 
 function startCapture(url, body) {
     $.ajax({
@@ -98,24 +44,7 @@ function startCapture(url, body) {
             updateStatus();
         },
         error: function(err) {
-            console.log(err);
-        }
-    });
-}
-
-function stopCapture(url, body) {
-    $.ajax({
-        url: domain + url,
-        type: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        data: JSON.stringify(body),
-        success: function() {
-            $("#lblStatus").html("Stopped Successfully.");
-            updateStatus();
-        },
-        error: function(err) {
+            $("#lblStatus").html("Startup failure.");
             console.log(err);
         }
     });
