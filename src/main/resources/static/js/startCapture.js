ed8a59f40887d9e61379c346398fbbf168e0807a
Devon Martin <devmart10@gmail.com>
2018-01-24 18:43:22 -0800

    won't redirect to dashboard. temp bug fix

--- src/main/resources/static/js/startCapture.js
+++ src/main/resources/static/js/startCapture.js
@@ -2,6 +2,8 @@ var domain = "http://localhost:8080";
 
 $(document).ready(function() {
     var now = new Date();
+    // TODO i think this is bc Pacific Time Zone
+    now.setHours(now.getHours() - 8);
     $("#txtStartTime").val(String(now.toISOString().replace("Z", "")));
     // TODO: fix bug about 24 + 2
     now.setHours(now.getHours() + 2);
@@ -17,7 +19,6 @@ $(document).ready(function() {
         if ($("#txtStartTime").val()) {
             endTime = String($("#txtEndTime").val());
         }
-
         var body = {
             id: $("#txtID").val(),
             rds: $("#txtRDS").val(),
@@ -41,7 +42,7 @@ function startCapture(url, body) {
         data: JSON.stringify(body),
         success: function() {
             $("#lblStatus").html("Started Successful.");
-            window.location.href = "dashboard";
+            // window.location.href = "dashboard";
         },
         error: function(err) {
             $("#lblStatus").html("Startup failure.");
