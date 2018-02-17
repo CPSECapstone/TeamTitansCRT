318901cd6f996822118ed1377f8d52fb6d5283d4
R3xKyle <kyle@ringler.com>
2018-02-17 13:57:46 -0800

    Added time parsing for capture logs, tests for logParser, and a db injection method for replay

--- src/main/java/Database.java
+++ src/main/java/Database.java
@@ -1,6 +1,7 @@
 import java.sql.SQLException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.Statement;
 
 public class Database {
     String dbms;
@@ -18,19 +19,45 @@ public class Database {
         this.password = password;
     }
 
-    public Connection getConnection() throws SQLException
+    public Connection getConnection()
     {
+        Connection conn = null;
         try
         {
             Class.forName("com.mysql.jdbc.Driver");
+
+            conn = DriverManager.getConnection("jdbc:" + this.dbms + "://" +
+                    this.dbURL + "/" + this.database, this.username, this.password);
+
         }
         catch (ClassNotFoundException e)
         {
             e.printStackTrace();
         }
-
-        Connection conn = DriverManager.getConnection("jdbc:" + this.dbms + "://" +
-                this.dbURL + "/" + this.database, this.username, this.password);
+        catch (SQLException se)
+        {
+            se.printStackTrace();
+        }
         return conn;
     }
+
+    public boolean query(String sqlQuery)
+    {
+        Connection conn = getConnection();
+        if (conn == null)
+        {
+            return false;
+        }
+        try
+        {
+            Statement stmt = conn.createStatement();
+            stmt.execute(sqlQuery);
+        }
+        catch (SQLException se) {
+            se.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
 }
