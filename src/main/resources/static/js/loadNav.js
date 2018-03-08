1e9795b1e42f1219af27c58622fa42d9819fb7ef
devmart10 <devmart10@gmail.com>
2018-03-07 21:34:45 -0800

    updates nav.js to inject html straight from code

--- src/main/resources/static/js/loadNav.js
+++ src/main/resources/static/js/loadNav.js
@@ -1,9 +1,46 @@
-// function that inject the nav html into any div with the placeholder class
-$.get("components/nav.html", function(data){
-    $("div.nav-placeholder").replaceWith(data);
+$(function() {
+    injectNavTop();
+    injectSidebar();
 });
 
+// function that inject the nav html into any div with the placeholder class
+function injectNavTop() {
+    $("div.nav-placeholder").replaceWith(`
+    <nav class="navbar navbar-custom z-depth-1" role="navigation">
+        <div id="nav-container">
+            <div class="navbar-header">
+                <a class="navbar-brand menu-brand page-scroll" href="#page-top">Menu</a>
+            </div>
+        </div>
+    </nav>`);
+}
+
 // function that inject the sidebar html into any div with the placeholder class
-$.get("components/sidebar.html", function(data){
-    $("div.sidebar-placeholder").replaceWith(data);
-});
+function injectSidebar() {
+    $("div.sidebar-placeholder").replaceWith(`
+    <nav class="navbar navbar-inverse navbar-fixed-top" id="sidebar-wrapper" role="navigation">
+        <ul class="nav sidebar-nav">
+            <li class="sidebar-brand">
+                <a href="dashboard">
+                    MyCRT
+                </a>
+            </li>
+            <li>
+                <a href="dashboard">Dashboard</a>
+            </li>
+            <li>
+                <a href="capture">My Captures</a>
+            </li>
+            <li>
+                <a href="replay">My Replays</a>
+            </li>
+            <li>
+                <a href="analyze">Analyze Results</a>
+            </li>
+            <li>
+                <a href="settings">Settings</a>
+            </li>
+        </ul>
+    </nav>`);
+}
+
